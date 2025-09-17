521a79a47b64b5ad3010d09e5243a8b8dfa96caa
Harpreet Sahota <sahota.singh.harpreet@gmail.com>
2025-09-17 11:53:38 -0700

    initial sketch:

---
+++ prep_dataset.py
@@ -0,0 +1,17 @@
+def download_dataset():
+    """
+    Download the OD_MetalDAM Dataset from source
+    """
+    pass
+
+def crop_dataset():
+    """
+    Crop images from  1280x895 to 1024x703 and remove the information band at the bottom
+    """
+    pass
+
+def sql_to_csv():
+    """
+    Parse MetalDAM_metadata.sql to csv for easier processing
+    """
+    pass
