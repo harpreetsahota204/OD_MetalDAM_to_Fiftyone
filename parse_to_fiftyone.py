c419769912c13cd1aa4ae89ebd0f43e806f576e2
Harpreet Sahota <sahota.singh.harpreet@gmail.com>
2025-09-17 12:59:52 -0700

    parse to one file

--- parse_to_fiftyone.py
+++ parse_to_fiftyone.py
@@ -1,5 +1,25 @@
 import fiftyone as fo
 
+def download_dataset():
+    """
+    Download the OD_MetalDAM Dataset from source
+    """
+    pass
+
+def crop_dataset():
+    """
+    Crop images from  1280x895 to 1024x703 and remove the information band at the bottom
+    and save to disk
+    """
+    pass
+
+def sql_to_csv():
+    """
+    Parse MetalDAM_metadata.sql to csv for easier processing
+    """
+    pass
+
+
 def parse_to_fiftyone():
     """
     """
@@ -8,6 +28,8 @@ def parse_to_fiftyone():
     dataset = fo.Dataset(name="OD_MetalDAM", overwrite=True)
 
     # parse FO samples
+
+    # probably  need to load csv and parse the metadata
 
 
     # load image from disk
